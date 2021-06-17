4dc0505afb738cd3fe88bde60421917e99e185e3
Ashley Bilbrey <44065883+AshleyBilbrey@users.noreply.github.com>
2021-06-16 18:04:12 -0600

    Add rendering basics

--- your-story.js
+++ your-story.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+app.set("view engine", "ejs");
+app.use(express.static("static"));
+
 app.get('/', (req, res) => {
-    res.send("This is the home page");
+    res.render("splash.ejs");
 })
 
 app.listen(port, () => {
